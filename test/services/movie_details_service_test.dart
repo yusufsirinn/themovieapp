9cab9c87a79718ad79dcaf2bc436f36ae0e6edea
Yusuf Sirin <yusufsirinn@gmail.com>
2022-07-23 21:23:45 +0300

    added test cases

---
+++ test/services/movie_details_service_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:themovieapp/models/movie_details_model.dart';
+import 'package:themovieapp/services/tmdb_movie_details_service/tmdb_movie_details_service.dart';
+
+void main() {
+  late TMDBMovieDetailsService service;
+  setUp(() {
+    service = TMDBMovieDetailsService();
+  });
+  test('Fetch movie wiht id', () async {
+    MovieDetails? response;
+    var fetchedId = 222;
+    await service.fetchMovieDetails(
+      id: fetchedId,
+      onSuccess: (res) {
+        response = res;
+      },
+      onError: () {},
+    );
+    expect(response, isNotNull);
+    expect(response?.id, fetchedId);
+  });
+}
