7457691b8dcae116e50f817c697843097619e41d
Yusuf Sirin <yusufsirinn@gmail.com>
2023-05-07 01:51:14 +0300

    edited movie details

--- lib/views/movie_details/bone/movie_details_view.dart
+++ lib/views/movie_details/bone/movie_details_view.dart
@@ -17,35 +17,38 @@ class _MovieDetailsViewState extends State<MovieDetailsView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MovieDetailsAppBar(title: context.read<MovieDetailsBloc>().state.movie?.title),
-      body: Center(
-        child: Stack(
-          children: [
-            const MovieBackdrop(),
-            SafeArea(
-              child: Padding(
-                padding: AppPadding.large.all,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const MoviePoster(),
-                    AppDimension.s.width,
-                    BlocBuilder<MovieDetailsBloc, MovieDetailsState>(builder: (context, state) {
-                      return ViewStateWidget(
-                        status: state.status,
-                        success: const MovieOverview(),
-                        failure: Center(
-                          child: Text(context.tr.somethingWentWrong),
-                        ),
-                        loading: const Expanded(child: Indicator()),
-                      );
-                    }),
-                  ],
+    return GestureDetector(
+      onVerticalDragEnd: (_) => AppNavigator.instance.pop(),
+      child: Scaffold(
+        appBar: MovieDetailsAppBar(title: context.read<MovieDetailsBloc>().state.movie?.title),
+        body: Center(
+          child: Stack(
+            children: [
+              const MovieBackdrop(),
+              SafeArea(
+                child: Padding(
+                  padding: AppPadding.large.all,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const MoviePoster(),
+                      AppDimension.s.width,
+                      BlocBuilder<MovieDetailsBloc, MovieDetailsState>(builder: (context, state) {
+                        return ViewStateWidget(
+                          status: state.status,
+                          success: const MovieOverview(),
+                          failure: Center(
+                            child: Text(context.tr.somethingWentWrong),
+                          ),
+                          loading: const Expanded(child: Indicator()),
+                        );
+                      }),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
