21190254636d4af0986348abc4fa21ae3fde18a1
Yusuf Sirin <yusufsirinn@gmail.com>
2023-05-11 23:27:03 +0300

    edited netvork manager

--- lib/core/managers/network/interceptors/app_log_interceptor.dart
+++ lib/core/managers/network/interceptors/app_log_interceptor.dart
@@ -20,7 +20,7 @@ class AppLogInterceptor extends Interceptor {
 
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
-    Logger.error(err.error.toString());
+    Logger.error(err.message.toString());
     return handler.next(err);
   }
 }
--- lib/core/managers/network/network_service.dart
+++ lib/core/managers/network/network_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart' hide Response;
+import 'package:dio/io.dart';
 import 'package:flutter/foundation.dart';
 
 import '../../core.dart';
@@ -10,15 +11,16 @@ typedef FutureResponse<T extends BaseResponseModel> = Future<Response<T>>;
 class NetworkManager {
   static NetworkManager? _instance;
   static NetworkManager get instance {
-    _instance ??= NetworkManager._init();
+    _instance ??= NetworkManager.init();
     return _instance!;
   }
 
   late Dio _client;
 
-  NetworkManager._init() {
+  NetworkManager.init({HttpClientAdapter? adapter}) {
     final baseOptions = BaseOptions(baseUrl: AppConstants.baseURL);
     _client = Dio(baseOptions);
+    _client.httpClientAdapter = adapter ?? IOHttpClientAdapter();
     _client.interceptors.addAll(
       [
         if (kDebugMode) AppLogInterceptor(),
@@ -31,11 +33,13 @@ class NetworkManager {
     required String path,
     required BaseResponseModel model,
     Map<String, dynamic>? queryParameters,
+    Method method = Method.get,
   }) async {
     RequestOptions requestOptions = RequestOptions(
       baseUrl: AppConstants.baseURL,
       path: path,
       queryParameters: queryParameters,
+      method: method.toString(),
     );
     try {
       var response = await _client.fetch(requestOptions);
@@ -45,8 +49,22 @@ class NetworkManager {
       } else {
         throw Exception(response.data);
       }
+    } on DioError catch (e) {
+      return Response(error: Error(message: e.message.toString()));
     } catch (e) {
       return Response(error: Error(message: e.toString()));
     }
   }
+}
+
+enum Method {
+  get(),
+  put(),
+  post(),
+  patch();
+
+  const Method();
+
+  @override
+  String toString() => name.toUpperCase();
 }
