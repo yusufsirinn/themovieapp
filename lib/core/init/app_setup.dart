40f42c124c07f9dd979db4b485cc7cb1f586ef08
Yusuf Sirin <yusufsirinn@gmail.com>
2023-05-14 13:54:06 +0300

    added firebase

--- lib/core/init/app_setup.dart
+++ lib/core/init/app_setup.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import '../../firebase_options.dart';
 import '../core.dart';
 
 class AppSetup {
   static init() async {
-    if (kDebugMode) Bloc.observer = const AppBlocObserver();
+    WidgetsFlutterBinding.ensureInitialized();
     await dotenv.load(fileName: '.env');
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    if (kDebugMode) Bloc.observer = const AppBlocObserver();
   }
 }
